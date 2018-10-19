7cc1acaa7c46fc9b2f8a5d3217869029c5416e04
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-10-19 17:11:10 +0200

    Z01R: Make Android.mk search recursive
    Change-Id: I3a05344f949eae581f1f17cc33b1df475d3e6760
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- Android.mk
+++ Android.mk
@@ -17,7 +17,8 @@
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter Z01R,$(TARGET_DEVICE)),)
-include $(call all-makefiles-under,$(LOCAL_PATH))
+  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 
 include $(CLEAR_VARS)
 
