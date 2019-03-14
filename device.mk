bf65f59ab1db7139205d3aa6097f1365a222ce4c
Jackeagle <jackeagle102@gmail.com>
2019-03-14 15:22:28 +0100

    Z01R: Bringup Light HAL
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- device.mk
+++ device.mk
@@ -82,6 +82,10 @@ PRODUCT_PACKAGES += \
     android.hidl.manager@1.0 \
 	android.hidl.manager@1.0_system
 
+# Lights
+PRODUCT_PACKAGES += \
+	android.hardware.light@2.0-service.asus_Z01R
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_vendor.xml:system/etc/media_profiles_vendor.xml
