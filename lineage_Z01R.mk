bf6eb0ff8cb0b4588d02d5ebfafddab7f067c1cd
TARKZiM <tom8476oo@gmail.com>
2019-06-24 17:06:36 +0800

    Z01R: lineage: fix manufacturer typo
    * also inherit product_launched_with_o.mk.

--- lineage_Z01R.mk
+++ lineage_Z01R.mk
@@ -17,6 +17,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 
 # Inherit from Z01R device
 $(call inherit-product, device/asus/Z01R/device.mk)
@@ -26,8 +27,8 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_Z01R
 PRODUCT_DEVICE := Z01R
-PRODUCT_MANUFACTURER := Asus
-PRODUCT_BRAND := Asus
+PRODUCT_MANUFACTURER := asus
+PRODUCT_BRAND := asus
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
