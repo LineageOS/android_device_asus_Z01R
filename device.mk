957c645c3cdfebdc1a8f59de6e481050f437ddd1
rohan <purohit.rohan@gmail.com>
2019-09-24 19:42:02 +0200

    [DNM] Bringup
    Change-Id: Id25635a49d1e719d902dca0f3aac657b47dddcac

--- device.mk
+++ device.mk
@@ -173,8 +173,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+#PRODUCT_BOOT_JARS += \
+#    telephony-ext
 
 # Trust HAL
 PRODUCT_PACKAGES += \
@@ -197,5 +197,5 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     libnl
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
+#PRODUCT_BOOT_JARS += \
+#    WfdCommon
