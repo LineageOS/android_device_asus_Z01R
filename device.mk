ad1bcfc56ab4e40e0cd1c8133f83940f29e451c6
Bruno Martins <bgcngm@gmail.com>
2019-10-08 14:56:41 +0100

    Z01R: Split RF configs from the main NFC config
    This is now expected with the Q NXP NFC halimp.
    
    Change-Id: Ie58c0cb61441e8bbe22ed100c1067a91a49921d1

--- device.mk
+++ device.mk
@@ -373,6 +373,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/libnfc-nxp_RF.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # Power
