6cda3b08f1ba481ddb9f82208fe326eff3c66f07
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-11-03 13:11:15 +0100

    Z01R: Update include paths for platform QCOM sepolicy
    Change-Id: Ic885875133a4ccdea0c6c152df7b1acda6158f7a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -153,8 +153,13 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # Sepolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/qcom/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/qcom/sepolicy/public
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/qcom/sepolicy/generic/private \
+    device/qcom/sepolicy/qva/private
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/qcom/sepolicy/generic/public \
+    device/qcom/sepolicy/qva/public
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
