40059de1eccf0d53854bc213ed6394ada5b5733a
rohan <purohit.rohan@gmail.com>
2020-02-06 09:21:30 -0500

    Z01R: Copy privapp-permissions-hotword to product
    Change-Id: I4dedb8b32d5925fd392474f70c34b3d06e9d5bab

--- device.mk
+++ device.mk
@@ -103,7 +103,7 @@ PRODUCT_PACKAGES += \
 
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:system/etc/permissions/privapp-permissions-hotword.xml
+    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/fts_ts.kl:system/usr/keylayout/fts_ts.kl \
