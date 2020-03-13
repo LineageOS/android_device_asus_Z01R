c247a152dfb6472ff597b4874a07f34b5c083351
Jack Pham <jackp@codeaurora.org>
2020-03-12 23:59:56 -0700

    Z01R: init.qcom.usb.rc: Consolidate init.msm.usb.configfs.rc
    Remove obsolete android_usb based property triggers now
    that configfs is always used. Merge in init.msm.configfs.rc
    so we now have just a single .rc init file.
    
    Also remove all other references to /sys/class/android_usb/*.
    
    Change-Id: I06eba8e715ccee5974dc9de8ac19df6c79ae0c45

--- rootdir/etc/init.qcom.usb.sh
+++ rootdir/etc/init.qcom.usb.sh
@@ -43,10 +43,6 @@ esoc_name=`cat /sys/bus/esoc/devices/esoc0/esoc_name 2> /dev/null`
 
 target=`getprop ro.board.platform`
 
-if [ -f /sys/class/android_usb/f_mass_storage/lun/nofua ]; then
-	echo 1  > /sys/class/android_usb/f_mass_storage/lun/nofua
-fi
-
 #
 # Override USB default composition
 #
