8fbbab3111fcc4746db8a81a39526dc5c5dc33b2
Jackeagle <jackeagle102@gmail.com>
2020-06-07 16:05:18 +0200

    Z01R: Remove redundant NoCutoutOverlay
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: Ia7b8e0bfa58175b96129e48182fab37898f654e5

--- device.mk
+++ device.mk
@@ -53,9 +53,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_PACKAGES += \
-    NoCutoutOverlay
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
