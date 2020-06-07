21a9f022db6bdd087754e06291cb3debe015e841
Jackeagle <jackeagle102@gmail.com>
2020-06-07 11:09:14 +0200

    Z01R: Import seccomp media policy
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: I786ab7e57beaecd3ae8f5065e48f8450d74f2838

--- device.mk
+++ device.mk
@@ -386,6 +386,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
