6b5c4e6d956ca9bce3a27cb037f2872298059629
Marc Bourgoin <themard85@gmail.com>
2020-07-13 06:50:11 -0600

    Z01R: Pick select OEM changes to init scripts
    From Z01R WW_user_100.10.107.87_20200520
    
    * Instead of commenting out some lines we should delete them
    
    Change-Id: I2896dc65a7f086335f6f659aa7a37e83e7045935

--- device.mk
+++ device.mk
@@ -150,6 +150,8 @@ PRODUCT_PACKAGES += \
 
 # Common init scripts
 PRODUCT_PACKAGES += \
+    fstab.qcom \
+    amp_calibration.sh \
     init.asus.rc \
     init.class_main.sh \
     init.msm.usb.configfs.rc \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,6 +1,22 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fstab.qcom
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := amp_calibration.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/amp_calibration.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.asus.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
