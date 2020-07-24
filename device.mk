ddc4ab99f1c67a468fa6fd23bec549ce93ca2661
bgcngm <bgcngm@gmail.com>
2020-07-24 05:22:11 +0200

    Z01R: Switch to common QTI power HAL
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: Idd1d45c68dbd55e5c868366621d284b4337da928

--- device.mk
+++ device.mk
@@ -367,8 +367,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service \
-    android.hardware.power@1.0-impl \
+    android.hardware.power@1.2-service-qti \
     power.sdm845
 
 # QTI
