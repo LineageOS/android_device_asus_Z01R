0abfce40569538f67d71ffcb5dd2513ec12661bc
Marc Bourgoin <themard85@gmail.com>
2020-08-27 23:18:59 -0600

    Z01R: Fix fingerprint blobs for source-built vendor
    * Remove depreciated dependencies
    * Build libstdc++ for vendor
    
    Change-Id: I30f4d298316f147e44c217fc55bbac529a544ce2

--- device.mk
+++ device.mk
@@ -246,7 +246,8 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1 \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1-service \
+    libstdc++.vendor
 
 # FM
 PRODUCT_PACKAGES += \
