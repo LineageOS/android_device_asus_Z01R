2429b6b0fc76d570c0da15eb7763410cf3dfda6e
Marc Bourgoin <themard85@gmail.com>
2020-12-02 19:50:51 -0700

    Z01R: Update blobs from WW 100.10.107.110
    from WW_user_100.10.107.110_20201118
    
    Change-Id: I174c53f746fa39f386502a67ce931349dd87aa91

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,7 +54,7 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
-BOARD_KERNEL_CMDLINE += buildv=WW_100.10.107.87
+BOARD_KERNEL_CMDLINE += buildv=WW_100.10.107.110
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
@@ -182,7 +182,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 TARGET_RIL_VARIANT := caf
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2020-05-05
+VENDOR_SECURITY_PATCH := 2020-11-18
 
 # Sepolicy
 include device/qcom/sepolicy/sepolicy.mk
