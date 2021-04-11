852f0871365578f49a660295a90b2d7dc3e4da17
Marc Bourgoin <themard85@gmail.com>
2021-04-11 00:12:09 -0600

    Z01R: Kang CNE and DPM from PNX
    From Nokia/Phoenix_00WW/PNX_sprout:11/RKQ1.200906.002/00WW_6_190:user/release-keys
    
    Also update a few other PNX blobs to this build
    
    Change-Id: I97150f0e5d79e25d7711736e0be20c81dbdd0377

--- extract-files.sh
+++ extract-files.sh
@@ -62,17 +62,9 @@ function blob_fixup() {
         "${PATCHELF}" --remove-needed libkeystore_binder.so "${2}"
         "${PATCHELF}" --remove-needed libkeymaster_messages.so "${2}"
         ;;
-    system_ext/etc/init/dpmd.rc)
-        # Move telephony packages to /system_ext
-        sed -i "s/\/system\/product\/bin\//\/system\/system_ext\/bin\//g" "${2}"
-        ;;
-    system_ext/etc/permissions/com.qti.dpmframework.xml | system_ext/etc/permissions/dpmapi.xml | system_ext/etc/permissions/qcrilhook.xml | system_ext/etc/permissions/telephonyservice.xml)
+    system_ext/etc/permissions/qcrilhook.xml | system_ext/etc/permissions/telephonyservice.xml)
         # Move telephony packages to /system_ext
         sed -i "s/\/product\/framework\//\/system_ext\/framework\//g" "${2}"
-        ;;
-    system_ext/lib64/libdpmframework.so)
-        # Use libcutils-v29.so for libdpmframework.so
-        sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
         ;;
     esac
 }
