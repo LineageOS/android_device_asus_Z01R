92762569bef9247e85bca2cf48c89c4d176076d8
Marc Bourgoin <themard85@gmail.com>
2021-07-08 02:43:18 -0600

    Z01R: Switch to prebuilt health HAL
    Unfortunately, it seems Asus went with some proprietary method to implement
    Quick charging that has modifications to the charger blob that also depends
    on proprietary changes to the health HAL, so to prevent QC from causing
    issues on our device, especially when offline charging? Let's switch to
    prebuilt health HAL and allow it to work as Asus inteded it.
    
    Change-Id: Ia514a71724fb507cb63260434eef39208e3bf541

--- device.mk
+++ device.mk
@@ -271,9 +271,6 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl:64 \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service \
     charger_res_images
 
 # HIDL
