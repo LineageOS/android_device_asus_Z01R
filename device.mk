cb52156a9d2f73205a3b652a857f1d3cf43681dc
Chenyang Zhong <zhongcy95@gmail.com>
2021-10-19 09:37:49 -0400

    Z01R: Turn off compressed apex
    Google introduced compressed apex on android 12 [1], and the flag is
    enabled by default in updatable_apex.mk [2]. Xiaomi Mi MIX 2S cannot boot
    compressed apex with encryption, so disable it.
    
    ref:
    [1] https://source.android.com/devices/tech/ota/apex#compressed-apex
    [2] https://android.googlesource.com/platform/build/+/4a1240de8a2ee5795a30df123058677d3999145e
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: Bruno Martins <bgcngm@gmail.com>
    Change-Id: I6b213b5d4a368adcf77cbecc699496285adf184c

--- device.mk
+++ device.mk
@@ -1,26 +1,15 @@
 #
-# Copyright (C) 2018-2020 The LineageOS Project
+# Copyright (C) 2018-2021 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# SPDX-License-Identifier: Apache-2.0
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-# Get non-open-source specific aspects
-$(call inherit-product, vendor/asus/Z01R/Z01R-vendor.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+PRODUCT_COMPRESSED_APEX := false
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
