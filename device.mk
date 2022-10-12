3861a078abf829c7b3b4e1858693a2a2e8e37bc3
Michael Bestas <mkbestas@lineageos.org>
2022-10-12 18:02:10 +0300

    Z01R: Drop libhidl shim
    No longer needed
    
    Change-Id: If3d8b3e81f6c6472f724b3dd890eb1179e58117d

--- device.mk
+++ device.mk
@@ -277,8 +277,6 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.allocator@1.0.vendor \
-    android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
     android.hidl.memory@1.0.vendor \
     libhidltransport \
     libhidltransport.vendor \
