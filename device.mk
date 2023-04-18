b64c03d9dca9feaf6a08060255f85a3b42782f21
Marc Bourgoin <themard85@gmail.com>
2023-04-18 17:34:12 -0600

    Z01R: Switch to source-built fingerprint service
    So it turns out we were building source-built but overwriting it with prebuilt...
    so let's just get rid of prebuilt and properly swap to source-built
    
    Change-Id: I069d38548d1ffc0e939edd5401b25c918b94f866

--- device.mk
+++ device.mk
@@ -234,7 +234,6 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1 \
     android.hardware.biometrics.fingerprint@2.1-service \
     libstdc++.vendor
 
