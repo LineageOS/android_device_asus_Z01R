5e132afe6495f03f644761d0123899ca6f8a9f60
micky387 <mickaelsaibi@free.fr>
2023-09-23 08:10:06 +0200

    Z01R: Import camxoverridesettings from stock
    Change-Id: I87ad6e00fc518a474244a35c57a8c9e815d2d16b

--- device.mk
+++ device.mk
@@ -181,6 +181,9 @@ PRODUCT_PACKAGES += \
     libgui_vendor \
     vendor.qti.hardware.camera.device@1.0
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/camera/camxoverridesettings.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camxoverridesettings.txt
+
 # Common init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom \
