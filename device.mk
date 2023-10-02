6f758f605a5c906d4191dd47cb59a951a30cffdf
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2023-10-02 09:15:08 +0530

    Z01R: Include hardware/google/interfaces and hardware/google/pixel
     * Required by power-libperfmgr
    
    Change-Id: I4053938614be26c7066ea83d97955739f39b4144

--- device.mk
+++ device.mk
@@ -430,6 +430,8 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
     vendor/qcom/opensource/usb/etc
 
 # Telephony
