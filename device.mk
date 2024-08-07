8524b47383a463a7d93eab9d9be9eb044ab934cd
ShevT <evgeny.shishkov@gmail.com>
2024-08-07 08:42:43 +0300

    Z01R: Switch to AIDL NXP NFC HAL
    Change-Id: Iff03bd7fc522b45b9caebb12860c5c851356a9a1

--- device.mk
+++ device.mk
@@ -355,12 +355,12 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2-service \
+    android.hardware.nfc-service.nxp \
     com.android.nfc_extras \
     Tag
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # Partitions
