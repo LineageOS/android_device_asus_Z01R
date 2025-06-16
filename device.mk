947c3433241ef6cb863533ca747df103965d9cb8
basamaryan <basam.aryan@gmail.com>
2025-06-16 11:19:48 -0400

    Z01R: Drop legacy ANT remnants
    QSSI only has platform/external/ant-wireless/hidl since a while
    
    Change-Id: Ibccfdc7e72efd3c9d3ff62bda3e729b99433a567

--- device.mk
+++ device.mk
@@ -93,10 +93,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService
-
 # Atrace
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
