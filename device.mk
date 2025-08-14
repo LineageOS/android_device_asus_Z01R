927ec9b833abe88cf13b6432938cf70e173c0db9
LuK1337 <priv.luk@gmail.com>
2025-08-14 23:47:18 +0200

    Z01R: Migrate to AIDL LiveDisplay HAL
    Change-Id: I4e8be836e94d442b40c63a1ac7dade3c9d9c784c

--- device.mk
+++ device.mk
@@ -306,7 +306,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay-service.sdm
 
 # Lineage Health
 PRODUCT_PACKAGES += \
